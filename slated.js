e83a0b2b1c10302bef5a0b3c087acf5f052347d3
andrewomacdonald <andrew.o.macdonald@gmail.com>
2016-05-26 22:23:02 -0700

    App works when running with node server, working on deployment now

--- slated.js
+++ slated.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 var routes = require('./server/server.js');
 
-app.get('/', routes, function(request, response){
+app.get('/', function(request, response){
   response.render(data);
 });
