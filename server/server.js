d619599b64141230723c3036bfcb39d8076ad675
andrewomacdonald <andrew.o.macdonald@gmail.com>
2016-05-26 22:25:36 -0700

    Fixed postinstall script

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var server = app.listen(port, function(){
-  var port = server.address().port;
-  console.log('listening on port' + port);
-});
 
 app.use('/', express.static(__dirname + '/../client'));
-app.use('/', express.static(__dirname + '/../bower_components'));
+
+app.listen(port, function() {
+  console.log('listening on port', port);
+});
